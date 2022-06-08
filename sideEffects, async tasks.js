5bf9b37e076730820f4767bf5402f30d77b04247
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-06-08 22:27:09 +0700

    use useeffect in component to send http put request to firebase to store current cart item

---
+++ sideEffects, async tasks.js
@@ -0,0 +1,4 @@
+// must not put side effects and async in reducers
+// 2 options are
+// 1. inside the component with useEffects()
+// 2. inside the action creators
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +7,14 @@ import Products from "./components/Shop/Products";
 
 const App = () => {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart.items);
+
+  useEffect(() => {
+    fetch("https://redux-cb79f-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
